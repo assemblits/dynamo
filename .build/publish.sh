0124fcaa134dd85c46990232179399cff462938d
Gilberto J Requena <gilbertojrequena@gmail.com>
2017-10-03 13:05:51 +0200

    Fix env variable, changea artifact publihser user

--- .build/publish.sh
+++ .build/publish.sh
@@ -2,4 +2,4 @@
 
 openssl aes-256-cbc -d -a -in .build/sec.enc -out secring.gpg -pass pass:"${SIGNING_DECRYPT_KEY}"
 
-./gradlew uploadArchives -PossrhUsername="${OOSRH_USERNAME}" -PossrhPassword="${OSSRH_PASSWORD}" -Psigning.keyId="${SIGNING_KEY}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile=secring.gpg
+./gradlew uploadArchives -PossrhUsername="${OSSRH_USERNAME}" -PossrhPassword="${OSSRH_PASSWORD}" -Psigning.keyId="${SIGNING_KEY}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile=secring.gpg
